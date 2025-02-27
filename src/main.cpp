3574e5484f7ed4a180aa6977b6c34394b73b68e0
habib <sherif@>
2025-02-27 23:19:32 +0300

    Initial commit: CMake project setup

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int add(int a, int b);
+int subtract(int a, int b);
+int main() {
+	int x = 9;
+	int y = 3;
+	std::cout << "sum(+): " << add(x, y) << std::endl;
+	std::cout << "difference(-): " << subtract(x, y) << std::endl;
+	return 0;
+}
---
+++ src/utils.cpp
@@ -0,0 +1,9 @@
+// Function add for adding two numbers
+int add(int a, int b) {
+    return a + b;
+}
+// Function subtract for subtracting the second number from the first
+int subtract(int a, int b) {
+    return a - b;
+}
+
